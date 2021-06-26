fb6e541d886fc1e1755f4deca13a1d9f630942d7
nonari <alejandro.budino@gmail.com>
2021-06-26 22:06:28 +0200

    colab

--- testing/train.py
+++ testing/train.py
@@ -9,9 +9,9 @@ from sklearn import metrics
 import numpy as np
 
 data_path = "/content/tfgoct/"
-loss_data_path = "/mydrive/FIC/TFG/train_unet_final/loss/"
-accuracy_data_path = "/mydrive/FIC/TFG/train_unet_final/accuracy/"
-model_data_path = "/mydrive/FIC/TFG/train_unet_final/models/"
+loss_data_path = "/mydrive/FIC/TFG/train_unet_final/pretrained/loss/"
+accuracy_data_path = "/mydrive/FIC/TFG/train_unet_final/pretrained/accuracy/"
+model_data_path = "/mydrive/FIC/TFG/train_unet_final/pretrained/models/"
 
 
 def train_net(net, device, isbi_dataset, epochs=175, batch_size=9, lr=0.00001):
