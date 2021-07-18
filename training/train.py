3a13af66d63a059e44d0f80f2efeed25915dc8a3
nonari <alejandro.budino@gmail.com>
2021-07-18 13:25:11 +0200

    fixed batch split

--- training/train.py
+++ training/train.py
@@ -37,6 +37,7 @@ def train_net(net, device, isbi_dataset, epochs=175, batch_size=9, lr=0.00001):
     for epoch in range(epochs):
         net.train()
 
+        acc_total = 0
         loss_total = 0
         for idx, (image, label) in enumerate(train_loader):
             image = image.to(device=device, dtype=torch.float32)
@@ -47,19 +48,21 @@ def train_net(net, device, isbi_dataset, epochs=175, batch_size=9, lr=0.00001):
             mask_pred[void_pos] = -1
             label = label.to(device=device, dtype=torch.float32)
             accuracy = metrics.accuracy_score(mask_pred.flatten(), mask_label.flatten(), normalize=True)
+            acc_total += accuracy
             #accuracy_layers = split_acc(mask_pred, mask_label)
             loss = criterion(pred, label)
             loss.backward()
             loss_total += loss.item()
             if idx % config.parts == 0:
                 batch_loss = loss_total / config.parts
-                print('Loss/train', loss.item())
-                print('Accuracy', accuracy)
+                batch_acc = acc_total / config.parts
+                print('Loss/train', batch_loss)
+                print('Accuracy', batch_acc)
                 data_file = f'train_unet_p{isbi_dataset.patient_left}.txt'
-                save_line((loss.item(), epoch), os.path.join(config.save_data_dir, "loss", data_file))
-                save_line((accuracy, epoch), os.path.join(config.save_data_dir, "accuracy", data_file))
+                save_line((batch_loss, epoch), os.path.join(config.save_data_dir, "loss", data_file))
+                save_line((batch_acc, epoch), os.path.join(config.save_data_dir, "accuracy", data_file))
                 if batch_loss <= best_loss:
-                    best_loss = loss_total / config.parts
+                    best_loss = batch_loss
                     model_file = f"best_model_p{isbi_dataset.patient_left}.pth"
                     torch.save(net.state_dict(), os.path.join(config.save_data_dir, "models", model_file))
                 optimizer.step()
@@ -72,7 +75,7 @@ def tt():
     pretraining = config.weights
     f_utils.create_skel(config.save_data_dir)
     for i in range(config.ini, config.end):
-        device = torch.device('cpu' if torch.cuda.is_available() else 'cpu')
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         net = smp.Unet(
             encoder_name=encoder,
             encoder_weights=pretraining,
